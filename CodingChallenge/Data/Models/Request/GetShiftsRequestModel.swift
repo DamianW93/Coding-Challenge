4a38e51fa6244711f2e0caa0d9881c182aaf3d70
Damian Włodarczyk <damian.wlodarczyk.dod@gmail.com>
2022-04-24 13:21:04 +0200

    Add hardcoded address to GetShiftsRequestModel

--- CodingChallenge/Data/Models/Request/GetShiftsRequestModel.swift
+++ CodingChallenge/Data/Models/Request/GetShiftsRequestModel.swift
@@ -10,6 +10,8 @@ import Foundation
 struct GetShiftsRequestModel: QueryEncodableRequest {
     // Type is hardcoded just for simplicity as we want to fetch just one day at a time.
     let type: String = "list"
+    // Hardcoded address as stated in readme
+    let address: String = "Dallas, TX"
     let start: Date
     let end: Date
 }
